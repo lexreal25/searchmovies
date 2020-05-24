4827648f63e5a9aa37828eaf5b658a91e1e00f0f
Anthony Alexis Adoasi <anthony.adoasi@callenssolutions.com>
2020-05-24 11:05:12 +0000

    added materialize css framework

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import SearchMovies from './Components/SearchMovies';
 import './App.css';
 
 function App() {
-  render(){
+
     return(
       <div className="container">
         <h2 className="title">Search Any Movie Here</h2>
         <SearchMovies />
       </div>
     )
-  }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import SearchMovies from './Components/SearchMovies'
 import * as serviceWorker from './serviceWorker';
 
 // class Main extends React.Component{
