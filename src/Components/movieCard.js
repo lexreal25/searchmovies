1f980a695e7805d483b9554f6b7a8dc4daa0570f
Daniel Nagai <daniel@shop-office.net>
2020-05-25 03:09:26 +0000

    updates made

--- src/Components/movieCard.js
+++ src/Components/movieCard.js
@@ -17,16 +17,6 @@ export default function MovieCard({movie}) {
             <br/>
             <p style={{fontSize: 14}}>{movie.overview.substring(0, 200)}...</p>
           </div>
-            {/*<img className="card--image"*/}
-            {/*    src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}*/}
-            {/*    alt={movie.title + ' poster'}*/}
-            {/*    />*/}
-            {/*<div className="card--content">*/}
-            {/*    <h4 className="card--title">{movie.title}</h4>*/}
-            {/*    <p><small>RELEASE DATE: {movie.release_date}</small></p>*/}
-            {/*    <p><small>RATING: {movie.vote_average}</small></p>*/}
-            {/*    <p className="card--desc">{movie.overview}</p>*/}
-            {/*</div>*/}
 
         </div>
             
